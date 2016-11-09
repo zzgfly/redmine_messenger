24f9e716b971733f50e740086b8c1aae07624f75
Ron <Ron@3dit.de>
2016-11-09 14:17:50 +0100

    ATTENTION: circumvent ssl cert...

--- lib/redmine_mattermost/listener.rb
+++ lib/redmine_mattermost/listener.rb
@@ -157,6 +157,7 @@ class MattermostListener < Redmine::Hook::Listener
 				client = HTTPClient.new
 				client.ssl_config.cert_store.set_default_paths
 				client.ssl_config.ssl_version = :auto
+				client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
 				client.post_async url, {:payload => params.to_json}
 			rescue Exception => e
 				Rails.logger.warn("cannot connect to #{url}")
