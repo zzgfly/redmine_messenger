56a3eef324f090ec3223801639b24b4da898788e
Alexander Meindl <a.meindl@alphanodes.com>
2017-05-25 12:20:11 +0200

    More secure route for settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # Redmine Messenger plugin for Redmine
 
 RedmineApp::Application.routes.draw do
-  match 'projects/:id/messenger_settings/:action', controller: 'messenger_settings', via: %i[get post put patch]
+  match 'projects/:id/messenger_settings/save',
+        to: 'messenger_settings#save',
+        via: %i[post put patch]
 end
