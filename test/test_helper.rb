a68e3a2fb1f7678c75b1d7a66ed9b19ab954891a
Alexander Meindl <a.meindl@alphanodes.com>
2017-06-28 15:21:30 +0200

    Add code coverage to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,18 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter
+]
+
+SimpleCov.start :rails do
+  root File.expand_path(File.dirname(__FILE__) + '/..')
+end
+
 module RedmineMessenger
   class TestCase
     include ActionDispatch::TestProcess
